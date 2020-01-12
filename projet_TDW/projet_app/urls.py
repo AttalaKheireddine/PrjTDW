bf72e2c42067830971263969be297c95257f879a
ATTALA Kheireddine <gk_attala@esi.dz>
2020-01-12 11:15:23 +0100

    made bootstrap and font awesome work offline - designing the base template

--- projet_TDW/projet_app/urls.py
+++ projet_TDW/projet_app/urls.py
@@ -4,9 +4,11 @@ from . import views
 app_name = "projet_app"
 
 urlpatterns = [
-    url(r'my-profile',),
-    url(r'/profiles/(?P<slug>[\w\d-]+)/$',),
-    url(r'translator-recruit',),
-    url(r"a-propos"),
-    url(r"")
+    url(r'my-profile$',views.home,name="my profile"),
+    url(r'profiles/(?P<slug>[\w\d-]+)$',views.home,name="profile"),
+    url(r'translator-recruit$',views.home,name="recruit"),
+    url(r"a-propos$",views.home,name="a propos"),
+    url(r"translators$",views.home,name="translators"),
+    url(r"translation-types$",views.home,name="types"),
+    url(r"^$",views.home,name="home")
 ]
